9d99959c52f20ce13fa3bae9fbe2870febdd7e6c
Tatyana Tarasova <umnova.panther@yandex.ru>
2024-07-11 09:58:19 +0500

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,27 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBookTest {
+    PhoneBook contacts;
+    @BeforeEach
+    void startTest () {
+        System.out.println("Начало теста");
+        contacts = new PhoneBook();
+    }
+    @AfterEach
+    void endTest () {
+        System.out.println("Конец теста");
+    }
+    @Test
+    void addTest() {
+        var expeated = 1;
+        var actual = contacts.add("Василий", "+79545255464");
+        Assertions.assertEquals(expeated, actual);
+    }
+
 }
